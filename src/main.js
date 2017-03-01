e55128375a7c8d8a24deea7be2beec6593f98193
Luis Vilches <luis@elmundoalreves.cl>
2017-03-01 18:24:51 -0300

    section video

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 //import App from './App.vue'
+import VueYouTubeEmbed from 'vue-youtube-embed'
+Vue.use(VueYouTubeEmbed)
 
 new Vue({
   el: '#app',
-  //render: h => h(App)
+  data: {
+    videoId: 'videoId',
+  },
+  methods: {
+    method (url) {
+      this.videoId = this.$youtube.getIdFromURL(url)
+      this.startTime = this.$youtube.getTimeFromURL(url)
+    }
+  }
 })
